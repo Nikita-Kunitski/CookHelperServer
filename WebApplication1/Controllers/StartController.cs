75f2036998c2ea77b9fe283d66cbfbbf55097456
Никита Куницкий <uedeck777@live.com>
2018-05-06 16:49:45 +0300

    last stabil commit

---
+++ WebApplication1/Controllers/StartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StartController : Controller
+    {
+        CookHelperEntities db = new CookHelperEntities();
+
+        // GET: Start
+        public ActionResult Index()
+        {
+            var dish = db.BOOK_DISH.Include(path=>path.RECIPEs);
+            return View(dish.ToList());
+        }
+    }
+}
