1e00d963c814811ce38e9f85b4d4fc6f52d9c3cb
Никита Куницкий <uedeck777@live.com>
2018-05-09 17:05:37 +0300

    Commit

--- WebApplication1/Controllers/SearchController.cs
+++ WebApplication1/Controllers/SearchController.cs
@@ -3,16 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class SearchController : Controller
     {
+        
         // GET: Search
         [HttpGet]
-        public ActionResult Index(string[] arg)
+        public ActionResult Index()
         {
-            return View();
+          
+            int index = 0;
+            List<string> index_ = new List<string>();//список id ингредиентов, которые были в запросе
+            using (CookHelperEntities db = new CookHelperEntities())
+            {
+                List<string>arr =Request.Params.AllKeys.ToList();//получить все ключи параметров запроса
+               
+                foreach (var item in arr)
+                {
+                    if (!item.Equals("ingredient" + (index + 1)))
+                        break;
+                    index_.Add(Request.Params[item]);
+                    index++;
+                }              
+                List<Statistics> statistics = new List<Statistics>();
+                List<RECIPE> recipe = db.RECIPEs.ToList();
+                List<INGREDIENT> ingred = db.INGREDIENTs.ToList();
+                var group_recipes = from item in recipe
+                                    group item by item.C_id_dish;
+                foreach (IGrouping<int, RECIPE> g in group_recipes)
+                {
+                    int index_coincidence = 0;
+                    foreach (var ingr_dish in g)
+                    {
+                        foreach (var item in index_)
+                        {
+
+                            if (ingred.ElementAt(ingr_dish.C_id_ingred - 1).name_ingredient.Equals(item))
+                                index_coincidence++;
+                        }
+                    }
+                    statistics.Add(new Statistics(g.Key, g.Count(), index_coincidence));
+                }
+
+
+            }
+           return View();
         }
     }
 }
---
+++ WebApplication1/Models/Statistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Statistics
+    {
+        public Statistics(int id_dish, int counter_ingred, int counter_coincidence)
+        {
+            this.id_dish = id_dish;
+            this.counter_ingred = counter_ingred;
+            this.counter_coincidence = counter_coincidence;
+        }
+        public int id_dish { get; set; }
+        public int counter_ingred { get; set; }
+        public int counter_coincidence { get; set; }
+
+        public override string ToString()
+        {
+            string str = "id_dish:" + id_dish + "\ncounter_ingred:" + counter_ingred + "\ncounter_coincidence:" + counter_coincidence + "\n";
+            return str;
+        }
+    }
+}
